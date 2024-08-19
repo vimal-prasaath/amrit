add9f4853fb94765b12f85da66b6a8eb6ce95f15
Venkat <venkatesh@regami.solutions>
2024-08-19 13:23:04 +0530

    doctors images mapping

--- src/components/OurDoctors/OurDoctors.jsx
+++ src/components/OurDoctors/OurDoctors.jsx
@@ -1,14 +1,26 @@
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography, Button, Link } from "@mui/material";
 
 import { DoctorCard } from "components";
 import { data } from "./data";
+import { useParams } from "react-router";
+import { Link as RouterLink } from "react-router-dom";
 
 const OurDoctors = ({ detailView }) => {
+  const { departId, id } = useParams();
+
+  console.log(departId, id);
+
+  const isGeneral = departId === "general";
+
   const defaultProps = { bgcolor: "#f6f6f6", py: 4, px: 10 };
 
   const containerProps = detailView ? {} : defaultProps;
 
-  const docs = detailView ? data : data.slice(0, 8);
+  const idToMap = isGeneral ? id : departId;
+
+  const docs = detailView
+    ? data.filter((item) => item.id === idToMap)
+    : data.slice(0, 8);
 
   return (
     <Box {...containerProps}>
@@ -25,52 +37,27 @@ const OurDoctors = ({ detailView }) => {
         px={detailView ? 0 : 6}
         py={detailView ? 0 : 5}
       >
-        {docs.map((item, index) => (
+        {(docs?.length ? docs : data).map((item, index) => (
           <Box key={index} px={1.5} mb={3}>
             <DoctorCard data={item} detailView={detailView} />
           </Box>
         ))}
-
-        {/* <Box px={1.5} mb={2}>
-          <DoctorCard detailView={detailView} />
-        </Box>
-
-        <Box px={1.5} mb={2}>
-          <DoctorCard detailView={detailView} />
-        </Box>
-
-        <Box px={1.5} mb={2}>
-          <DoctorCard detailView={detailView} />
-        </Box>
-
-        <Box px={1.5} mb={2}>
-          <DoctorCard detailView={detailView} />
-        </Box>
-        <Box px={1.5} mb={2}>
-          <DoctorCard detailView={detailView} />
-        </Box>
-
-        <Box px={1.5} mb={2}>
-          <DoctorCard detailView={detailView} />
-        </Box>
-
-        <Box px={1.5} mb={2}>
-          <DoctorCard detailView={detailView} />
-        </Box>
-
-        <Box px={1.5} mb={2}>
-          <DoctorCard detailView={detailView} />
-        </Box> */}
       </Box>
       {!detailView && (
         <Box textAlign="center">
-          <Button
-            variant="contained"
-            color="primary"
-            sx={{ mr: 2.5, py: 1.25, minWidth: "12.5rem" }}
+          <Link
+            to="/our-doctor"
+            component={RouterLink}
+            sx={{ textDecoration: "none" }}
           >
-            View All Team
-          </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ mr: 2.5, py: 1.25, minWidth: "12.5rem" }}
+            >
+              View All Team
+            </Button>
+          </Link>
         </Box>
       )}
     </Box>
--- src/components/OurDoctors/data.js
+++ src/components/OurDoctors/data.js
@@ -1,5 +1,5 @@
 import d1 from "assets/images/doctors/1.jpg";
-import d2 from "assets/images/doctors/2.jpg";
+import d2 from "assets/images/doctors/23.jpg";
 import d3 from "assets/images/doctors/3.jpg";
 import d4 from "assets/images/doctors/4.jpg";
 import d5 from "assets/images/doctors/5.jpg";
@@ -22,30 +22,53 @@ import d20 from "assets/images/doctors/20.jpg";
 import d21 from "assets/images/doctors/21.jpg";
 import d22 from "assets/images/doctors/22.jpg";
 
+export const departmentConstants = {
+  OPTHOMOLOGY: "opthomology",
+  ORTHO: "ortho",
+  PEDIATRICS: "pediatrics",
+  GYNO: "gyno",
+  DERMA: "dermatology",
+  INTERNAL_MEDICINE: "internalMedicine",
+  DIBETOLOGY: "diabetology",
+  GEN_SURGERY: "generalSurgery",
+  ENT: "ent",
+  CARDIO: "cardiology",
+  NEPHRO: "nephrology",
+  NEURO: "neurology",
+  PHYSIO: "physiotherapy",
+  GASTRO: "gastroenterology",
+  RADIO: "radiology",
+  CC: "criticalCare",
+};
+
 export const data = [
   {
     name: "Dr. M. Sohanraj",
     specialist: "Senior Ophthalmology",
     hospitals: "Amrit Hospitals",
     img: d2,
+    id: departmentConstants.OPTHOMOLOGY,
   },
   {
     name: "Dr. Lalit Kumar S",
     specialist: "Chief Cataract, Cornea, Refractive Surgeon",
     hospitals: "Amrit Hospital",
     img: d16,
+    id: departmentConstants.OPTHOMOLOGY,
   },
   {
     name: "Dr. Vimala Sohanraj",
     specialist: "Obstetrics & Gynaecology",
     hospitals: "Amrit Hospitals",
     img: d9,
+    id: departmentConstants.GYNO,
   },
   {
     name: "Dr. Shalini Lalit Kumar",
     specialist: "Consultant Obstetrics & Gynaecology",
     hospitals: "Amrit hospitals",
     img: d20,
+    id: departmentConstants.GYNO,
   },
 
   {
@@ -53,24 +76,28 @@ export const data = [
     specialist: "Consultant Physician",
     hospitals: "Amrit Hospitals",
     img: d21,
+    id: departmentConstants.PHYSIO,
   },
   {
     name: "Dr. Chetan Kumar",
     specialist: "Consultant Pediatrician & Neonatologist",
     hospitals: "Amrit Hospitals",
     img: d12,
+    id: departmentConstants.PEDIATRICS,
   },
   {
     name: "Dr. S. Rajesh Kumar",
     specialist: "FVRS Vitreo - Retinal surgeon",
     hospitals: "Amrit Hospitals",
     img: d1,
+    id: departmentConstants.OPTHOMOLOGY,
   },
   {
     name: "Dr. Rakesh Kothari",
     specialist: "Consultant pediatrician & Neonatologist",
     hospitals: "Amrit Hospitals",
     img: d13,
+    id: departmentConstants.PEDIATRICS,
   },
 
   {
@@ -78,18 +105,21 @@ export const data = [
     specialist: "Internal medicine, Diabetology, Endocrine",
     hospitals: "Amrit Medical Centre",
     img: d2,
+    id: departmentConstants.DIBETOLOGY,
   },
   {
     name: "Dr. Ashish Chopra",
     specialist: "Consultant Cardiologist",
     hospitals: "Amrit Hospitals",
     img: d19,
+    id: departmentConstants.CARDIO,
   },
   {
     name: "Dr. Elizabeth",
     specialist: "Consultant physiotherapist",
     hospitals: "Amrit medical centre",
     img: d22,
+    id: departmentConstants.PHYSIO,
   },
 
   {
@@ -98,12 +128,14 @@ export const data = [
       "Senior Consultant Anaesthesiologist & Intensivist, Interventional pain management specialist",
     hospitals: "Amrit medical centre",
     img: d15,
+    id: departmentConstants.GEN_SURGERY,
   },
   {
     name: "Dr. Gowri",
     specialist: "Consultant Pathologist",
     hospitals: "Amrit Hospitals",
     img: d14,
+    id: departmentConstants.GEN_SURGERY,
   },
 
   {
@@ -111,36 +143,42 @@ export const data = [
     specialist: "Consultant Anaesthetic",
     hospitals: "Amrit Hospitals",
     img: d11,
+    id: departmentConstants.GEN_SURGERY,
   },
   {
     name: "Dr. Roshan Kumar",
     specialist: "Consultant Pulmonologist",
     hospitals: "Amrit Hospitals",
     img: d10,
+    id: departmentConstants.GEN_SURGERY,
   },
   {
     name: "Dr. Vikas Chandrakant",
     specialist: "General & Laparoscopic surgeon",
     hospitals: "Amrit Hospitals",
     img: d5,
+    id: departmentConstants.GEN_SURGERY,
   },
   {
     name: "Dr. Madhavi Yadav",
     specialist: "ENT surgeon",
     hospitals: "Amrit medical centre",
     img: d8,
+    id: departmentConstants.ENT,
   },
   {
     name: "Dr. Bharathi L",
     specialist: "Knee & Hip Replacement surgeon",
     hospitals: "Amrit Hospitals",
     img: d6,
+    id: departmentConstants.ORTHO,
   },
   {
     name: "Dr. Suraj J. Babar",
     specialist: "Sports, Implant Orthopaedic surgeon",
     hospitals: "Amrit Hospitals",
     img: d7,
+    id: departmentConstants.ORTHO,
   },
 
   {
@@ -148,12 +186,14 @@ export const data = [
     specialist: "General physician",
     hospitals: "Amrit Hospitals",
     img: d4,
+    id: departmentConstants.GEN_SURGERY,
   },
   {
     name: "Dr. O. Chandrakant",
     specialist: "General & Laparoscopic surgeon",
     hospitals: "Amrit Hospitals",
     img: d3,
+    id: departmentConstants.GEN_SURGERY,
   },
 
   {
@@ -161,5 +201,6 @@ export const data = [
     specialist: "Pediatric Orthopaedics Deformity correction",
     hospitals: "Amrit Medical centre",
     img: d17,
+    id: departmentConstants.ORTHO,
   },
 ];
